6d3199facc8afbe6ceab7f2bc90aae9bdaaeee03
chipets123 <sommylight145@gmail.com>
2025-02-11 17:19:02 +0000

    winning-project

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $menu_items = [
             color: #ff6347;
         }
         .slider {
-            width: 100%;
+            width: 800px;
             height: 400px;
             overflow: hidden;
             position: relative;
@@ -45,70 +45,13 @@ $menu_items = [
             object-fit: cover;
             animation: slide 30s infinite;
         }
-        @keyframes slider {
-
-0% {
-    left: 0;
-}
-
-20% {
-    left: 0;
-}
-
-25% {
-    left: -100%;
-}
-
-45% {
-    left: -100%;
-
-}
-
-50% {
-    left: -200%;
-
-}
-
-70% {
-    left: -200%;
-}
-
-75% {
-    left: -300%;
-}
-
-95% {
-    left: -300%;
-}
-
-100% {
-    left: -400%;
-
-}
-
-}
-
-#slider {
-    overflow: hidden;
-
-}
-
-#slider figure img {
-    width:20%;
-    float: left;
-
-}
-
-#slider figure {
-    position: relative;
-    width: 500%;
-    margin: 0;
-    left: 0;
-    text-align: left;
-    font-size: 0;
-    animation: 20s slider infinite; 
-
-}
+        @keyframes slide {
+            0% {transform: translateX(0);}
+            25% {transform: translateX(-100%);}
+            50% {transform: translateX(-200%);}
+            75% {transform: translateX(-300%);}
+            100% {transform: translateX(0);}
+        }
         .grid {
             display: grid;
             grid-template-columns: repeat(3, 1fr);
